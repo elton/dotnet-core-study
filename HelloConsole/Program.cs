d494375091a536b9730e5b237229ec1ab4aed835
Elton Zheng <elton.zheng.sh@gmail.com>
2021-12-11 11:15:50 +0800

    Completed the HelloConsole project.

---
+++ HelloConsole/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var currentDate = DateTime.Now;
+Console.WriteLine($"The current time is: {currentDate:t}");
