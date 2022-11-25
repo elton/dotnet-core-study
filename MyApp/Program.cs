174b09e2f1aaf5fed3c0b5ca0454bfe02def5ed8
Elton Zheng <elton.zheng.sh@gmail.com>
2022-11-25 18:53:02 +0800

    initial the project.

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,5 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
-var currentDate = DateTime.Now;
-Console.WriteLine($"The current time is: {currentDate:t}");
+Console.WriteLine("The current time is " + DateTime.Now);
